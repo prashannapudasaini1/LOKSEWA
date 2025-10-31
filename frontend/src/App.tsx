42aa44404fccc351eba68e77ab323ed7cbf32afd
rohangrg0 <grgrohan004@gmail.com>
2025-10-31 22:57:04 +0545

    Login and register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Loginpage from "./pages/login";
+import Registerpage from "./pages/register";
 import "./index.css";
 
 const App: React.FC = () => {
@@ -8,6 +10,8 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<Loginpage />} />
+        <Route path="register" element={<Registerpage />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,29 @@
+// RegisterPage.tsx
+import React from "react";
+import RegisterComp from "../components/RegisterComp";
+// replace with your PNG path
+import Navbar from "../components/Navbar"; 
+// import Footer from "../components/Footer"; 
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div className="bg-[#0A0A0F] min-h-screen flex flex-col">
+      {/* Navbar */}
+      <Navbar />
+
+    <div className="flex-1 flex items-center justify-center px-6">
+      {/* Left Side: Glowing Image */}
+
+
+      {/* Right Side: Register Component */}
+      <div className="md:w-1/2 flex justify-center items-center p-10">
+        <RegisterComp />
+      </div>
+    
+    </div>
+
+    </div>
+  );
+};
+
+export default RegisterPage;
